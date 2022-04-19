70593daed4681d1960057518cb2caab70e50a52a
JPszC <JPszC.dev@gmail.com>
2022-04-19 00:08:30 -0400

    add wallpapers

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -15,10 +15,10 @@ numlockx on &
 #blueman-applet &
 #flameshot &
 #picom --config $HOME/.config/picom/picom.conf &
-picom --config .config/picom/picom-blur.conf --experimental-backends &
+picom --config ~/.config/picom/picom-blur.conf --experimental-backends &
 #/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 dunst &
-feh --randomize --bg-fill /usr/share/wallpapers/garuda-wallpapers/*
+feh --randomize --bg-fill ~/themes/wallpapers/*
 #starting user applications at boot time
 run volumeicon &
 #run discord &
