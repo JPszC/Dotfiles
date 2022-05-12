0d4a60afd2fe2b10288471332ac835a24c158b7e
JPszC <JPszC@pm.me>
2022-05-12 11:16:30 -0400

    update

---
+++ misc/focal-server-cloudimg.sh
@@ -0,0 +1,28 @@
+#!/bin/sh 
+
+SRC_IMG="https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64-disk-kvm.img"
+IMG_NAME="focal-server-cloudimg-amd64-disk-kvm.qcow2"
+wget -O $IMG_NAME $SRC_IMG
+
+apt update
+apt install -y libguestfs-tools
+virt-customize --install qemu-guest-agent -a $IMG_NAME
+
+TEMPL_NAME="ubuntu2004-cloud"
+VMID="9000"
+MEM="512"
+DISK_SIZE="32G"
+DISK_STOR="local-lvm"
+NET_BRIDGE="vmbr0"
+qm create $VMID --name $TEMPL_NAME --memory $MEM --net0 virtio,bridge=$NET_BRIDGE
+qm importdisk $VMID $IMG_NAME $DISK_STOR
+qm set $VMID --scsihw virtio-scsi-pci --scsi0 $DISK_STOR:vm-$VMID-disk-0
+qm set $VMID --ide2 $DISK_STOR:cloudinit
+qm set $VMID --boot c --bootdisk scsi0
+qm set $VMID --serial0 socket --vga serial0
+qm set $VMID --ipconfig0 ip=dhcp
+qm resize $VMID scsi0 $DISK_SIZE
+qm template $VMID
+# Remove downloaded image
+rm $IMG_NAME
+
